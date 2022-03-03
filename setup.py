3faae1662588dd6f82e554bde59014b9f45a348b
Soojung Kim <soojung.dev@gmail.com>
2022-03-03 09:55:09 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch-tutorial',
+    packages=find_packages(),
+    version=f'{datetime.now():%Y%m%d}',
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'python-snappy==0.6.0',
+        'pyarrow==4.0.1',
+        'fastparquet==0.5.0',
+        'pandas==1.1.5',
+        'transformers==4.5.1',
+        'albumentations',
+        'faiss',
+        'sklearn',
+        'opencv-python',
+    ],
+)
